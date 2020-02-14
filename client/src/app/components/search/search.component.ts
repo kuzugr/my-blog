d2c5dcd1706b787b4d3e4e591b9d2441d02a584c
kuzugr <monmon.syusyo@gmail.com>
2020-02-14 16:28:50 +0900

    レイアウト変更を進めた

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../shared/services/category.service';
+import { Category } from '../../shared/models/category';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+  categories: Array<Category>;
+  categoryLoaded: boolean;
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryLoaded = false;
+    this.loadCategories();
   }
 
+  async loadCategories() {
+    this.categoryService.loadCategories().then((categories) => {
+      if (!!categories) {
+        this.categories = categories;
+        this.categoryLoaded = true;
+      }
+    });
+  }
 }
