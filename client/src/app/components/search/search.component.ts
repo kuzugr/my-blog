5001800e3283dcb5c3cd6f91d0d2d7bf57df5dcb
kuzugr <monmon.syusyo@gmail.com>
2020-02-17 13:45:58 +0900

    タイトルをトップへ遷移できるようaタグに変更。フリーワードによる記事検索機能を追加。全体的にaタグをhrefではなくrouterlinkに変更

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../shared/services/category.service';
 import { Category } from '../../shared/models/category';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ValidateForm } from '../../shared/functions/validate-forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -10,11 +13,25 @@ import { Category } from '../../shared/models/category';
 export class SearchComponent implements OnInit {
   categories: Array<Category>;
   categoryLoaded: boolean;
+  form: FormGroup;
+  formErrors: { [key: string]: Array<string> } = {};
+  validationMessages = {
+    keyword: {
+      required: 'キーワードを入力してください。',
+      maxlength: 'キーワードは50文字以内で入力してください。',
+    },
+  };
 
-  constructor(private categoryService: CategoryService) { }
+  constructor(
+    private categoryService: CategoryService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
     this.categoryLoaded = false;
+    this.form = new FormGroup({
+      keyword: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    });
     this.loadCategories();
   }
 
@@ -25,5 +42,14 @@ export class SearchComponent implements OnInit {
         this.categoryLoaded = true;
       }
     });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.formErrors = {};
+      this.router.navigateByUrl(`/search?keyword=${encodeURIComponent(this.form.value['keyword'])}#top`);
+    } else {
+      this.formErrors = ValidateForm(this.form, false, this.validationMessages);
+    }
   }
 }
