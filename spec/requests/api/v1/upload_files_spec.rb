73242dd1dab6064af1510285047f9865e8db2018
kuzugr <monmon.syusyo@gmail.com>
2020-02-23 22:52:12 +0900

    upload_files_controllerのテストに認証を追加

--- spec/requests/api/v1/upload_files_spec.rb
+++ spec/requests/api/v1/upload_files_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UploadFilesController, type: :request do
   let!(:image) { 'spec/images/Desert.jpg' }
+  let!(:current_user) { create(:user, id: 1) }
 
   describe 'POST /api/v1/upload_files' do
     context 'parameterが正しい場合' do
       before do
+        allow_any_instance_of(Api::V1::ArticlesController).to receive(:published_option).and_return([true, false])
         uplaod_image =  fixture_file_upload(image, "image/jpeg")
         post '/api/v1/upload_files', params: { image: uplaod_image }
       end
@@ -18,11 +20,22 @@ RSpec.describe Api::V1::UploadFilesController, type: :request do
 
     context '不正なparameterの場合' do
       before do
+        allow_any_instance_of(Api::V1::ArticlesController).to receive(:published_option).and_return([true, false])
         uplaod_image =  fixture_file_upload(image, "image/jpeg")
         post '/api/v1/upload_files'
       end
       it '500エラーが返る' do
         expect(response.code).to eq '500'
+      end
+    end
+
+    context '未認証の場合' do
+      before do
+        uplaod_image =  fixture_file_upload(image, "image/jpeg")
+        post '/api/v1/upload_files'
+      end
+      it '401エラーが返る' do
+        expect(response.code).to eq '401'
       end
     end
   end
